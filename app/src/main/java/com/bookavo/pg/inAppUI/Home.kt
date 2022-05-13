ddc72a79684e9540a6b248bc5900d1879c0da70b
Miguel Hernández Gaitan <miguelhernandezgaitan13@gmail.com>
2022-05-12 19:30:27 -0600

    Splash screen funcionando :D

--- app/src/main/java/com/bookavo/pg/inAppUI/Home.kt
+++ app/src/main/java/com/bookavo/pg/inAppUI/Home.kt
@@ -5,16 +5,18 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.view.get
 import androidx.fragment.app.Fragment
 import com.bookavo.pg.databinding.HomeBinding
-import com.google.android.material.tabs.TabLayout
-
+import com.bookavo.pg.databinding.CardDailyBinding
+import com.google.firebase.firestore.FirebaseFirestore
 
 /**
  * A simple [Fragment] subclass as the default destination in the navigation.
  */
 class Home : Fragment() {
 
+    private val db = FirebaseFirestore.getInstance()
     private var _binding: HomeBinding? = null
 
     // This property is only valid between onCreateView and
@@ -34,6 +36,22 @@ class Home : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        // insert layout
+        db.collection("books").get().addOnSuccessListener { result ->
+            for (document in result) {
+                val category = document.data.get("category").toString()
+                val summary = document.data.get("summary").toString()
+                val image = "@drawable/" + document.data.get("img").toString()
+                val title = document.data.get("title").toString()
+
+                val cardDailyBinding = CardDailyBinding.inflate(layoutInflater)
+//                println(cardDailyBinding.root.)
+
+                // insert card
+//                val card = LayoutInflater.from(context).inflate(R.layout.card_daily, null)
+                binding.linearDailyList.addView(cardDailyBinding.root)
+            }
+        }
 //        val tabs = activity!!.findViewById<View>(R.id.content_main_tabs) as TabLayout
 
 //        binding.buttonFirst.setOnClickListener {
