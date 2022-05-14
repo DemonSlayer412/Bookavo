96a60ea8f9204f17fa7aec8d5f58e4e3179ec4f6
DemonSlayer412 <enriqueu734@gmail.com>
2022-05-13 22:21:24 -0600

    Agregado gif en splash screen (testeo)

--- app/src/main/java/com/bookavo/splashscreen/MainActivity.kt
+++ app/src/main/java/com/bookavo/splashscreen/MainActivity.kt
@@ -1,13 +1,21 @@
 package com.bookavo.splashscreen
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageView
+import androidx.appcompat.app.AppCompatActivity
 import com.bookavo.pg.R
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
 
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        //imagen del libro-cate
+        val url = "https://imgur.com/FaMIJQl"
+
+
     }
 }
