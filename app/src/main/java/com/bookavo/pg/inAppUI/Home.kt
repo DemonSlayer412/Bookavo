45058384a3c0b0419ffe37e025afa41dec7c8fd6
Miguel Hernández Gaitan <miguelhernandezgaitan13@gmail.com>
2022-05-15 08:35:04 -0600

    Renderizando cards :D

--- app/src/main/java/com/bookavo/pg/inAppUI/Home.kt
+++ app/src/main/java/com/bookavo/pg/inAppUI/Home.kt
@@ -1,15 +1,20 @@
 package com.bookavo.pg.inAppUI
 
-import android.R
+import android.graphics.drawable.Drawable
+import android.os.Build
 import android.os.Bundle
+import android.os.StrictMode
+import android.os.StrictMode.ThreadPolicy
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.core.view.marginRight
 import androidx.fragment.app.Fragment
-import com.bookavo.pg.databinding.HomeBinding
 import com.bookavo.pg.databinding.CardDailyBinding
+import com.bookavo.pg.databinding.HomeBinding
 import com.google.firebase.firestore.FirebaseFirestore
+import java.io.IOException
+import java.net.URL
+
 
 /**
  * A simple [Fragment] subclass as the default destination in the navigation.
@@ -28,16 +33,31 @@ class Home : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        if (Build.VERSION.SDK_INT > 9) {
+            val policy = ThreadPolicy.Builder().permitAll().build()
+            StrictMode.setThreadPolicy(policy)
+        }
 
         // insert layout
         db.collection("books").get().addOnSuccessListener { result ->
             for (document in result) {
                 val category = document.data.get("category").toString()
-                val summary = document.data.get("summary").toString()
-                val image = "@drawable/" + document.data.get("img").toString()
+                var summary = document.data.get("summary").toString()
+                if(summary.length > 100) {
+                    summary = summary.substring(0, 100)
+                }
+                val image = document.data.get("img").toString()
                 val title = document.data.get("title").toString()
                 val cardDailyBinding = CardDailyBinding.inflate(layoutInflater)
                 val cardDailyRoot = cardDailyBinding.root
+
+                // insert data
+                cardDailyBinding.bookCategory.text = category
+                cardDailyBinding.bookTitle.text = title
+                cardDailyBinding.bookSummary.text = summary
+                cardDailyBinding.containerInfo.background = loadImageFromNetwork(image)
+//                cardDailyBinding.containerInfo.background = context?.getDrawable(resources.getIdentifier(image, null, null))
+
                 // insert card
                 binding.linearDailyList.addView(cardDailyRoot)
 //                cardDailyRoot.card
@@ -52,5 +72,22 @@ class Home : Fragment() {
 
     fun onClickCard(view: View) {
         println("click card")
+    }
+
+    private fun loadImageFromNetwork(imageUrl: String): Drawable? {
+        var drawable: Drawable? = null
+        try {
+            drawable = Drawable.createFromStream(
+                URL(imageUrl).openStream(), "1eAzB6g.png"
+            )
+        } catch (e: IOException) {
+            println("test" + e.message)
+        }
+        if (drawable == null) {
+            println("Es nula")
+        } else {
+            println("No es nula")
+        }
+        return drawable
     }
 }
