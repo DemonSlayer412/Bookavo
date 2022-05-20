bbe357020b0c3a90f90194418d5532ff166e2935
DemonSlayer412 <enriqueu734@gmail.com>
2022-05-19 22:18:31 -0600

    Contraseña requerida menos estricta Cambio de nombre de botones (iniciar sesion y crear cuenta) de login y register Modo oscuro en construccion

--- app/src/main/java/com/bookavo/pg/Register.kt
+++ app/src/main/java/com/bookavo/pg/Register.kt
@@ -81,9 +81,6 @@ class Register : AppCompatActivity() {
         val passwordRegex = Pattern.compile(
             "^" +
                     "(?=.*[0-9])" +         //at least 1 digit
-                    "(?=.*[a-z])" +        //at least 1 lower case letter
-                    "(?=.*[A-Z])" +        //at least 1 upper case letter
-                    "(?=.*[@#$%^&+=])" +    //at least 1 special character
                     "(?=\\S+$)" +           //no white spaces
                     ".{6,}" +               //at least 6 characters
                     "$"
